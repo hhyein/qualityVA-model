eb238f0969d5ea6a01638e152c5f85699f731149
hhyein <48901291+hhyein@users.noreply.github.com>
2022-03-21 00:09:16 +0900

    Update Charttable.js

--- templates/src/components/Charttable.js
+++ templates/src/components/Charttable.js
@@ -1,34 +1,8 @@
 import React from 'react'
 import Barchart1 from './Barchart1'
 
-const exampleData = [
-  {
-    key: 'column 1',
-    missing: <Barchart1 />,
-    outlier: <Barchart1 />,
-    incons: <Barchart1 />,
-    'quantile statistics': <Barchart1 />,
-    'descriptive statistics': <Barchart1 />,
-  },
-  {
-    key: 'column 2',
-    missing: <Barchart1 />,
-    outlier: <Barchart1 />,
-    incons: <Barchart1 />,
-    'quantile statistics': <Barchart1 />,
-    'descriptive statistics': <Barchart1 />,
-  },
-  {
-    key: 'column 3',
-    missing: <Barchart1 />,
-    outlier: <Barchart1 />,
-    incons: <Barchart1 />,
-    'quantile statistics': <Barchart1 />,
-    'descriptive statistics': <Barchart1 />,
-  },
-]
 export default function Charttable(props) {
-  const { data = exampleData, onClick } = props
+  const { data = [], onClick } = props
 
   return data.length > 0 ? (
     <table border={1}>
