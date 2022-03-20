4eb6de8872a4d5d579beba966774f3be408218e5
hhyein <48901291+hhyein@users.noreply.github.com>
2022-03-20 18:28:26 +0900

    Update Charttable.js

--- templates/src/components/Charttable.js
+++ templates/src/components/Charttable.js
@@ -1,40 +1,15 @@
-import React from 'react'
-import Linechart from './Linechart'
+import React from 'react';
+import Barchart1 from './Barchart1';
 
-/*
-  data 형식이
-  [
-    {
-      key: string(row 이름)
-      missing: 차트
-      incons: 차트
-      .
-      .
-      .
-      .
-    },
-    ...
-  ]
-  
-  missing이나 incons는 예시임 table column 이름들을 넣어주면 됨
-*/
 const exampleData = [
   {
-    key: 'row1',
-    missing: (
-      <Linechart
-        data={[
-          { date: 'A', unemployed: 70 },
-          { date: 'B', unemployed: 50 },
-          { date: 'C', unemployed: 90 },
-        ]}
-      />
-    ),
-    outlier: <Linechart data={[]} />,
-    incons: <Linechart data={[]} />,
-    'quantile statistics': <Linechart data={[]} />,
-    'descriptive statistics': <Linechart data={[]} />,
-  },
+    key: 'column 1',
+    missing: <Barchart1 />,
+    outlier: <Barchart1 />,
+    incons: <Barchart1 />,
+    'quantile statistics': <Barchart1 />,
+    'descriptive statistics': <Barchart1 />,
+  }
 ]
 export default function Charttable(props) {
   const { data = exampleData } = props
