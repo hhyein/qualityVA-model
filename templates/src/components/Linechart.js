e4b13bf5ed3874adb8302ef3bba95709e8914d1e
hhyein <48901291+hhyein@users.noreply.github.com>
2022-03-20 22:44:28 +0900

    Update Linechart.js

--- templates/src/components/Linechart.js
+++ templates/src/components/Linechart.js
@@ -12,51 +12,62 @@ function Linechart(props) {
     d3.select(svgRef.current).selectAll("*").remove();
 
     var margin = {top: 10, right: 30, bottom: 30, left: 60},
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+      width = 500 - margin.left - margin.right,
+      height = 200 - margin.top - margin.bottom;
 
+    // append the svg object to the body of the page
     svg
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
 
-    var sumstat = d3.nest()
-      .key(function(d) { return d.date;})
-      .entries(data);
+    //Read the data
+    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv", function(data) {
 
-    var x = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) { return d.date; }))
-      .range([ 0, width ]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).ticks(5));
+      // group the data: I want to draw one line per group
+      var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+        .key(function(d) { return d.name;})
+        .entries(data);
 
-    var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.unemployed; })*5])
-      .range([ height, 0 ]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
+      // Add X axis --> it is a date format
+      var x = d3.scaleLinear()
+        .domain(d3.extent(data, function(d) { return d.year; }))
+        .range([ 0, width ]);
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).ticks(5));
 
-    var res = sumstat.map(function(d){ return d.key })
-    var color = d3.scaleOrdinal()
-      .domain(res)
-      .range(['#e41a1c','#377eb8','#4daf4a'])
+      // Add Y axis
+      var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return +d.n; })])
+        .range([ height, 0 ]);
+      svg.append("g")
+        .call(d3.axisLeft(y));
 
-    svg
-      .selectAll(".line")
-      .data(sumstat)
-      .enter()
-      .append("path")
-      .attr("fill", "none")
-      .attr("stroke", function(d){ return color(d.key) })
-      .attr("stroke-width", 1.5)
-      .attr("d", function(d){
-        return d3.line()
-          .x(function(d) { return x(d.date); })
-          .y(function(d) { return y(+d.unemployed); })
-          (d.values)
-      })
+      // color palette
+      var res = sumstat.map(function(d){ return d.key }) // list of group names
+      var color = d3.scaleOrdinal()
+        .domain(res)
+        .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+
+      // Draw the line
+      svg.selectAll(".line")
+          .data(sumstat)
+          .enter()
+          .append("path")
+            .attr("fill", "none")
+            .attr("stroke", function(d){ return color(d.key) })
+            .attr("stroke-width", 1.5)
+            .attr("d", function(d){
+              return d3.line()
+                .x(function(d) { return x(d.year); })
+                .y(function(d) { return y(+d.n); })
+                (d.values)
+            })
+
+    })
     }, [props.data]);
 
   return (
