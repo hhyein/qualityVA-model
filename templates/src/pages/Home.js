a6999179ba03f596e4f3df5a7bb841a6ff8db7d9
hhyein <48901291+hhyein@users.noreply.github.com>
2022-03-21 00:11:26 +0900

    Update Home.js

--- templates/src/pages/Home.js
+++ templates/src/pages/Home.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+
 import Tablechart from '../components/Tablechart'
-import Barchart1 from '../components/Barchart1'
-import Barchart2 from '../components/Barchart2'
 import Charttable from '../components/Charttable'
 
 import Treechart from '../components/Treechart'
-
-import Histogramchart1 from '../components/Histogramchart1'
-import Histogramchart2 from '../components/Histogramchart2'
 import Correlationchart from '../components/Correlationchart'
 import Scatterchart from '../components/Scatterchart'
-
 import Spiderchart from '../components/Spiderchart'
-import Densitychart2 from '../components/Densitychart2'
-import Boxchart2 from '../components/Boxchart2'
 import Linechart from '../components/Linechart'
+
+import Barchart1 from '../components/Barchart1'
+import Barchart2 from '../components/Barchart2'
+import Boxchart1 from '../components/Boxchart1'
+import Boxchart2 from '../components/Boxchart2'
+import Densitychart1 from '../components/Densitychart1'
+import Densitychart2 from '../components/Densitychart2'
+import Histogramchart1 from '../components/Histogramchart1'
+import Histogramchart2 from '../components/Histogramchart2'
 
 import { mainLayout1Style, mainLayout2Style } from '../const'
 
@@ -24,6 +26,8 @@ const PORT = 5000
 const Home = () => {
   const [mainStyle, setMainStyle] = useState(mainLayout1Style)
   const [data, setData] = useState([])
+  const [data1, setData1] = useState([])
+  const [data2, setData2] = useState([])
   const [selectedCharttablePos, setSelectedCharttablePos] = useState({
     row: 0,
     col: 0,
@@ -36,8 +40,31 @@ const Home = () => {
           Math.random()
       )
       .then(response => {
-        console.log(response.data)
         setData(response.data)
+      })
+      .catch(error => {
+        alert(`ERROR - ${error.message}`)
+      })
+
+      axios
+      .get(
+        `http://${window.location.hostname}:${PORT}/static/barchart1.json?` +
+          Math.random()
+      )
+      .then(response => {
+        setData1(response.data)
+      })
+      .catch(error => {
+        alert(`ERROR - ${error.message}`)
+      })
+
+      axios
+      .get(
+        `http://${window.location.hostname}:${PORT}/static/barchart2.json?` +
+          Math.random()
+      )
+      .then(response => {
+        setData2(response.data)
       })
       .catch(error => {
         alert(`ERROR - ${error.message}`)
@@ -57,11 +84,37 @@ const Home = () => {
         </div>
         <div className="box" style={{ gridArea: 'horizontal-bar-chart' }}>
           <div>
-            <Barchart1 />
-            <Barchart2 />
+            <Barchart1 data={data1} />
+            <Barchart2 data={data2} />
           </div>
           <div>
             <Charttable
+              data={[
+                {
+                  key: 'column 1',
+                  missing: <Barchart1 data={data1} />,
+                  outlier: <Barchart1 data={data1} />,
+                  incons: <Barchart1 data={data1} />,
+                  'quantile statistics': <Barchart1 data={data1} />,
+                  'descriptive statistics': <Barchart1 data={data1} />
+                },
+                {
+                  key: 'column 2',
+                  missing: <Barchart1 data={data1} />,
+                  outlier: <Barchart1 data={data1} />,
+                  incons: <Barchart1 data={data1} />,
+                  'quantile statistics': <Barchart1 data={data1} />,
+                  'descriptive statistics': <Barchart1 data={data1} />
+                },
+                {
+                  key: 'column 3',
+                  missing: <Barchart1 data={data1} />,
+                  outlier: <Barchart1 data={data1} />,
+                  incons: <Barchart1 data={data1} />,
+                  'quantile statistics': <Barchart1 data={data1} />,
+                  'descriptive statistics': <Barchart1 data={data1} />
+                }
+              ]}
               onClick={(rowIdx, colIdx) =>
                 setSelectedCharttablePos({ row: rowIdx, col: colIdx })
               }
@@ -98,8 +151,6 @@ const Home = () => {
         </div>
         <div className="box" style={{ gridArea: 'other-chart' }}>
           <Spiderchart data={data} />
-          <Spiderchart data={data} />
-          {/* attribute d: Expected number, "M0,NaNL0.3999999999…". */}
           <Densitychart2 data={data} />
           <Boxchart2 data={data} />
           <Linechart data={data} />
@@ -110,3 +161,5 @@ const Home = () => {
 }
 
 export default Home
+// bar chart: Error: <rect> attribute width: Expected length, "NaN".
+// histogram: Error: <rect> attribute width: A negative value is not valid. ("-1")
