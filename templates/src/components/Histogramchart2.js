f50d382cf04c0e6d4da434d822b714818c0301df
hhyein <48901291+hhyein@users.noreply.github.com>
2022-03-22 17:54:07 +0900

    Update Histogramchart2.js

--- templates/src/components/Histogramchart2.js
+++ templates/src/components/Histogramchart2.js
@@ -9,12 +9,10 @@ function Histogramchart(props) {
     var svg = d3.select(svgRef.current);
     d3.select(svgRef.current).selectAll("*").remove();
 
-    // set the dimensions and margins of the graph
-    var margin = {top: 10, right: 30, bottom: 30, left: 40},
-        width = 400 - margin.left - margin.right,
-        height = 80 - margin.top - margin.bottom;
+    var margin = { top: 20, right: 20, bottom: 20, left: 20 },
+      width = 300 - margin.left - margin.right,
+      height = 80 - margin.top - margin.bottom;
 
-    // append the svg object to the body of the page
     svg
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -22,34 +20,28 @@ function Histogramchart(props) {
         .attr("transform",
               "translate(" + margin.left + "," + margin.top + ")");
 
-    // get the data
     d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function(data) {
 
-      // X axis: scale and draw:
       var x = d3.scaleLinear()
-          .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+          .domain([0, 1000])
           .range([0, width]);
       svg.append("g")
           .attr("transform", "translate(0," + height + ")")
           .call(d3.axisBottom(x));
 
-      // set the parameters for the histogram
       var histogram = d3.histogram()
-          .value(function(d) { return d.price; })   // I need to give the vector of value
-          .domain(x.domain())  // then the domain of the graphic
-          .thresholds(x.ticks(70)); // then the numbers of bins
+          .value(function(d) { return d.price; })
+          .domain(x.domain())
+          .thresholds(x.ticks(70));
 
-      // And apply this function to data to get the bins
       var bins = histogram(data);
 
-      // Y axis: scale and draw:
       var y = d3.scaleLinear()
           .range([height, 0]);
-          y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+          y.domain([0, d3.max(bins, function(d) { return d.length; })]);
       svg.append("g")
           .call(d3.axisLeft(y));
 
-      // append the bar rectangles to the svg element
       var myHistogram = svg.selectAll("rect")
           .data(bins)
           .enter()
