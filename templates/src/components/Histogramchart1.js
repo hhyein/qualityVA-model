1bc36a67b4126aae000f08d2a9e9c0e75b0094b6
hhyein <48901291+hhyein@users.noreply.github.com>
2022-03-22 17:53:16 +0900

    Update Histogramchart1.js

--- templates/src/components/Histogramchart1.js
+++ templates/src/components/Histogramchart1.js
@@ -9,95 +9,126 @@ function Histogramchart1(props) {
     var svg = d3.select(svgRef.current)
     d3.select(svgRef.current).selectAll('*').remove()
 
-    // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 30, bottom: 30, left: 40 },
-      width = 400 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom
-
-    // append the svg object to the body of the page
+    var data = []
+    var ordinals = []
+    
+    for(var i = 0; i < 100; i++){
+      data.push({
+        value: Math.random()*10,
+        city: 'test' + i
+      })
+      ordinals.push('test' + i)
+    }
+    
+    var margin = { top: 20, right: 20, bottom: 20, left: 20 },
+      width = 350 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom - 80,
+      height2 = 30
+    
     svg
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
+      .attr('width', 550)
+      .attr('height', 400)
+    
+    var focus = svg.append('g')
+      .attr('class', 'focus')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    
+    var context = svg.append('g')
+      .attr('class', 'context')
+      .attr('transform', 'translate(' + margin.left + ',' + (margin.top + 300) + ')')
+    
+    var x = d3.scaleLinear().range([0, width])
+    var x2 = d3.scaleLinear().range([0, width])
+    var y = d3.scaleLinear().range([height, 0])
+    var y2 = d3.scaleLinear().range([height2, 0])
+    
+    var xBand = d3.scaleBand().domain(d3.range(-1, ordinals.length)).range([0, width])
+    
+    var xAxis = d3.axisBottom(x).tickFormat((d, e) => ordinals[d])
+    var xAxis2 = d3.axisBottom(x2)
+    var yAxis = d3.axisLeft(y)
+    var yAxis2 = d3.axisLeft(y2)
+    
+    var brush = d3.brushX()
+        .extent([[0, 0], [width, height2]])
+        .on('brush', brushed)
+    
+    x.domain([-1, ordinals.length])
+    y.domain([0, d3.max(data, d => d.value)])
+    x2.domain(x.domain())
+    y2.domain([0, d3.max(data, d => d.value)])
+    
+    focus.append('g')
+         .attr('clip-path','url(#my-clip-path)')
+         .selectAll('.bar')
+         .data(data)
+         .enter()
+         .append('rect')
+         .attr('class', 'bar')
+         .attr('x', (d, i) => {
+          return x(i) - xBand.bandwidth()*0.9/2
+         })
+         .attr('y', (d, i) => {
+          return y(d.value)
+         })
+         .attr('width', xBand.bandwidth()*0.9)
+         .attr('height', d => {
+          return height - y(d.value)
+         })
+    
+    focus.append('g')
+          .attr('class', 'axis')
+          .attr('transform', 'translate(' + 0 + ',' + height + ')')
+          .call(xAxis)
+    
+    focus.append('g')
+          .attr('class', 'axis axis--y')
+          .call(yAxis)
+    
+    var defs = focus.append('defs')
+    
+    defs.append('clipPath')
+        .attr('id', 'my-clip-path')
+        .append('rect')
+        .attr('width', width)
+        .attr('height', height)
+    
+    context.selectAll('.bar')
+         .data(data)
+         .enter()
+         .append('rect')
+         .attr('class', 'bar')
+         .attr('x', (d, i) => {
+          return x2(i) - xBand.bandwidth()*0.9/2
+         })
+         .attr('y', (d, i) => y2(d.value))
+         .attr('width', xBand.bandwidth()*0.9)
+         .attr('height', d => {
+          return height2 - y2(d.value)
+         })
+    
+    context.append('g')
+          .attr('class', 'axis2')
+          .attr('transform', 'translate(' + 0 + ',' + height2 + ')')
+          .call(xAxis)
+    
+    context.append('g')
+          .attr('class', 'brush')
+          .call(brush)
+          .call(brush.move, x.range())
+    
+    function brushed() {
+      var s = d3.event.selection || x2.range()
+      console.log(x.invert(s[0]))
+      console.log(x.invert(s[1]))
 
-    // get the data
-    d3.csv(
-      'https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv',
-      function (data) {
-        // X axis: scale and draw:
-        var x = d3
-          .scaleLinear()
-          .domain([0, 1000]) // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-          .range([0, width])
-        svg
-          .append('g')
-          .attr('transform', 'translate(0,' + height + ')')
-          .call(d3.axisBottom(x))
-
-        // set the parameters for the histogram
-        var histogram = d3
-          .histogram()
-          .value(function (d) {
-            return d.price
-          }) // I need to give the vector of value
-          .domain(x.domain()) // then the domain of the graphic
-          .thresholds(x.ticks(70)) // then the numbers of bins
-
-        // And apply this function to data to get the bins
-        var bins = histogram(data)
-
-        // Y axis: scale and draw:
-        var y = d3.scaleLinear().range([height, 0])
-        y.domain([
-          0,
-          d3.max(bins, function (d) {
-            return d.length
-          }),
-        ]) // d3.hist has to be called before the Y axis obviously
-        svg.append('g').call(d3.axisLeft(y))
-
-        // append the bar rectangles to the svg element
-        var myHistogram = svg
-          .selectAll('rect')
-          .data(bins)
-          .enter()
-          .append('rect')
-          .attr('x', 1)
-          .attr('transform', function (d) {
-            return 'translate(' + x(d.x0) + ',' + y(d.length) + ')'
-          })
-          .attr('width', function (d) {
-            return x(d.x1) - x(d.x0) - 1
-          })
-          .attr('height', function (d) {
-            return height - y(d.length)
-          })
-          .style('fill', function (d) {
-            if (d.x0 > 700) {
-              return 'red'
-            } else {
-              return '#69b3a2'
-            }
-          })
-
-        // Add brushing
-        svg.call(
-          d3
-            .brush()
-            .extent([
-              [0, 0],
-              [400, 400],
-            ])
-            .on('start brush', updateChart)
-        )
-
-        function updateChart() {
-          var extent = d3.event.selection
-          console.log(extent)
-        }
-      }
-    )
+      x.domain(s.map(x2.invert, x2))
+      focus.select('.axis').call(xAxis)
+      focus.selectAll('.bar')
+           .attr('x', (d, i) => {
+            return x(i) - xBand.bandwidth()*0.9/2
+           })
+          }
   }, [props.data])
 
   return (
