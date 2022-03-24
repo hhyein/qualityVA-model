3b431cddd999a03a094d22c6818a720d40cbab51
hhyein <48901291+hhyein@users.noreply.github.com>
2022-03-24 16:40:22 +0900

    Update Boxchart2.js

--- templates/src/components/Boxchart2.js
+++ templates/src/components/Boxchart2.js
@@ -1,32 +1,27 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
 function Boxchart2(props) {
-  const {data} = props;
-  const svgRef = useRef();
-  const d3 = window.d3v4;
+  const {data} = props
+  const svgRef = useRef()
+  const d3 = window.d3v4
 
   useEffect(() => {
     var svg = d3.select(svgRef.current);
     d3.select(svgRef.current).selectAll("*").remove();
 
-    // set the dimensions and margins of the graph
     var margin = {top: 10, right: 30, bottom: 30, left: 40},
-        width = 300 - margin.left - margin.right,
-        height = 300 - margin.top - margin.bottom;
+      width = 300 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
 
-    // append the svg object to the body of the page
     svg
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
       .append("g")
-        .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-    // Read the data and compute summary statistics for each specie
     d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv", function(data) {
-
-      // Compute quartiles, median, inter quantile range min and max --> these info are then used to draw the box.
-      var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+      var sumstat = d3.nest()
         .key(function(d) { return d.Species;})
         .rollup(function(d) {
           var q1 = d3.quantile(d.map(function(g) { return g.Sepal_Length;}).sort(d3.ascending),.25)
@@ -39,7 +34,6 @@ function Boxchart2(props) {
         })
         .entries(data)
 
-      // Show the X scale
       var x = d3.scaleBand()
         .range([ 0, width ])
         .domain(["setosa", "versicolor", "virginica"])
@@ -49,26 +43,23 @@ function Boxchart2(props) {
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x))
 
-      // Show the Y scale
       var y = d3.scaleLinear()
         .domain([3,9])
         .range([height, 0])
       svg.append("g").call(d3.axisLeft(y))
 
-      // Show the main vertical line
       svg
         .selectAll("vertLines")
         .data(sumstat)
         .enter()
         .append("line")
-          .attr("x1", function(d){return(x(d.key))})
-          .attr("x2", function(d){return(x(d.key))})
-          .attr("y1", function(d){return(y(d.value.min))})
-          .attr("y2", function(d){return(y(d.value.max))})
-          .attr("stroke", "black")
-          .style("width", 40)
+        .attr("x1", function(d){return(x(d.key))})
+        .attr("x2", function(d){return(x(d.key))})
+        .attr("y1", function(d){return(y(d.value.min))})
+        .attr("y2", function(d){return(y(d.value.max))})
+        .attr("stroke", "black")
+        .style("width", 40)
 
-      // rectangle for the main box
       var boxWidth = 100
       svg
         .selectAll("boxes")
@@ -82,27 +73,25 @@ function Boxchart2(props) {
             .attr("stroke", "black")
             .style("fill", "#69b3a2")
 
-      // Show the median
       svg
         .selectAll("medianLines")
         .data(sumstat)
         .enter()
         .append("line")
-          .attr("x1", function(d){return(x(d.key)-boxWidth/2) })
-          .attr("x2", function(d){return(x(d.key)+boxWidth/2) })
-          .attr("y1", function(d){return(y(d.value.median))})
-          .attr("y2", function(d){return(y(d.value.median))})
-          .attr("stroke", "black")
-          .style("width", 80)
+        .attr("x1", function(d){return(x(d.key)-boxWidth/2) })
+        .attr("x2", function(d){return(x(d.key)+boxWidth/2) })
+        .attr("y1", function(d){return(y(d.value.median))})
+        .attr("y2", function(d){return(y(d.value.median))})
+        .attr("stroke", "black")
+        .style("width", 80)
     })
-
     }, [props.data]);
     
   return (
     <>
-      <svg ref={svgRef}>
+      <svg ref = {svgRef}>
       </svg>
     </>
   );
 }
-export default Boxchart2;
+export default Boxchart2
