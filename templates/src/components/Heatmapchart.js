ca2688da0f25a610d33d50a7a04ef8c5c41187b4
hhyein <48901291+hhyein@users.noreply.github.com>
2022-03-24 16:41:23 +0900

    Update Heatmapchart.js

--- templates/src/components/Heatmapchart.js
+++ templates/src/components/Heatmapchart.js
@@ -1,34 +1,28 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
 function Heatmapchart(props) {
-  const {data} = props;
-  console.log(data);
-
-  const svgRef = useRef();
-  const d3 = window.d3v4;
+  const {data} = props
+  const svgRef = useRef()
+  const d3 = window.d3v4
 
   useEffect(() => {
     var svg = d3.select(svgRef.current);
     d3.select(svgRef.current).selectAll("*").remove();
 
-    // set the dimensions and margins of the graph
     var margin = {top: 30, right: 30, bottom: 30, left: 30},
       width = 200 - margin.left - margin.right,
       height = 200 - margin.top - margin.bottom;
 
-    // append the svg object to the body of the page
     svg
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+      .append("g")
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-    // Labels of row and columns
     var myGroups = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
     var myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"]
 
-    // Build X scales and axis:
     var x = d3.scaleBand()
       .range([ 0, width ])
       .domain(myGroups)
@@ -37,7 +31,6 @@ function Heatmapchart(props) {
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x))
 
-    // Build X scales and axis:
     var y = d3.scaleBand()
       .range([ height, 0 ])
       .domain(myVars)
@@ -45,7 +38,6 @@ function Heatmapchart(props) {
     svg.append("g")
       .call(d3.axisLeft(y));
 
-    // Build color scale
     var myColor = d3.scaleLinear()
       .range(["white", "#69b3a2"])
       .domain([1,100])
@@ -63,10 +55,9 @@ function Heatmapchart(props) {
     }, [props.data]);
 
   return (
-    <>
-      <svg ref={svgRef}>
-      </svg>
-    </>
+    <div className = "svg-wrapper">
+      <svg ref = {svgRef}></svg>
+    </div>
   );
 }
-export default Heatmapchart;
+export default Heatmapchart
