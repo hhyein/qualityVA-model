9e5a39a5891174e866d1e3f0d970c5e91e4eb549
hhyein <48901291+hhyein@users.noreply.github.com>
2022-03-29 12:57:31 +0900

    Update imputation.py

--- module/imputation.py
+++ module/imputation.py
@@ -11,3 +11,26 @@ def LowerUpper(df):
     lower, upper = q25 - cut_off, q75 + cut_off
 
     return lower, upper
+
+def ecdfDf(df, index):
+    tmpIndex, tmpX, tmpY = [], [], []
+    data = {}
+
+    if type(df) == 'pandas.core.frame.DataFrame':
+        x = np.sort(df.to_numpy())
+    else:
+        x = np.sort(df)
+        
+    y = 1. * np.arange(len(x))/float(len(x) - 1)
+
+    for i in range(0,len(x)):
+        tmpIndex.append(index)
+        tmpX.append(x[i])
+        tmpY.append(y[i])
+
+    data['index'] = tmpIndex
+    data['x'] = tmpX
+    data['y'] = tmpY
+
+    outputDf = pd.DataFrame(data)
+    return outputDf
