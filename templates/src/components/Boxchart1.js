909ce3d8531f0c379edf796e43a2aca4081f18d4
hhyein <48901291+hhyein@users.noreply.github.com>
2022-04-01 15:30:16 +0900

    Update Boxchart1.js

--- templates/src/components/Boxchart1.js
+++ templates/src/components/Boxchart1.js
@@ -5,13 +5,16 @@ function Boxchart1(props) {
   const svgRef = useRef()
   const d3 = window.d3v4
 
+  var df = []
+  Object.assign(df, data[0].Value)
+
   useEffect(() => {
     var svg = d3.select(svgRef.current);
     d3.select(svgRef.current).selectAll("*").remove();
 
-    var margin = {top: 10, right: 30, bottom: 30, left: 40},
-      width = 200 - margin.left - margin.right,
-      height = 200 - margin.top - margin.bottom;
+    var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+      width = 150 - margin.left - margin.right,
+      height = 50 - margin.top - margin.bottom
 
     svg
       .attr("width", width + margin.left + margin.right)
@@ -20,9 +23,7 @@ function Boxchart1(props) {
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-    var data = [12,19,11,13,12,22,13,4,15,16,18,19,20,12,11,9]
-
-    var data_sorted = data.sort(d3.ascending)
+    var data_sorted = df.sort(d3.ascending)
     var q1 = d3.quantile(data_sorted, .25)
     var median = d3.quantile(data_sorted, .5)
     var q3 = d3.quantile(data_sorted, .75)
@@ -30,28 +31,36 @@ function Boxchart1(props) {
     var min = q1 - 1.5 * interQuantileRange
     var max = q1 + 1.5 * interQuantileRange
 
-    var y = d3.scaleLinear()
-      .domain([0,24])
-      .range([height, 0]);
-    svg.call(d3.axisLeft(y))
+    var x = d3.scaleLinear()
+      .domain([min, max])
+      .range([0, width])
+    svg
+      .append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x))
 
-    var center = 100
-    var width = 100
+    var y = d3.scaleBand()
+      .range([0, height])
+    svg.append('g')
+      .call(d3.axisLeft(y))
+
+    var height = 20
+    var center = 15
 
     svg
     .append("line")
-    .attr("x1", center)
-    .attr("x2", center)
-    .attr("y1", y(min) )
-    .attr("y2", y(max) )
+    .attr("x1", x(min) )
+    .attr("x2", x(max) )
+    .attr("y1", center )
+    .attr("y2", center )
     .attr("stroke", "black")
 
     svg
     .append("rect")
-    .attr("x", center - width/2)
-    .attr("y", y(q3) )
-    .attr("height", (y(q1)-y(q3)) )
-    .attr("width", width )
+    .attr("x", x(q1) )
+    .attr("y", center - height/2 )
+    .attr("height", height )
+    .attr("width", (x(q3) - x(q1)) )
     .attr("stroke", "black")
     .style("fill", "#69b3a2")
 
@@ -60,17 +69,18 @@ function Boxchart1(props) {
     .data([min, median, max])
     .enter()
     .append("line")
-    .attr("x1", center-width/2)
-    .attr("x2", center+width/2)
-    .attr("y1", function(d){ return(y(d))} )
-    .attr("y2", function(d){ return(y(d))} )
+    .attr("x1", function(d){ return(x(d))} )
+    .attr("x2", function(d){ return(x(d))} )
+    .attr("y1", center-height/2 )
+    .attr("y2", center+height/2 )
     .attr("stroke", "black")
     }, [props.data]);
     
   return (
-    <div className = "svg-wrapper">
-      <svg ref = {svgRef}></svg>
-    </div>
+    <>
+      <svg ref = {svgRef}>
+      </svg>
+    </>
   );
 }
 export default Boxchart1
