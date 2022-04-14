8ae62c97e91387e5fd779460a84fb79f24aa2ee5
hhyein <48901291+hhyein@users.noreply.github.com>
2022-04-14 13:46:47 +0900

    Update Home.js

--- templates/src/pages/Home.js
+++ templates/src/pages/Home.js
@@ -2,35 +2,41 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import Tablechart from '../components/Tablechart'
-import Charttable from '../components/Charttable'
-
 import Linechart from '../components/Linechart'
+import NL4DV from '../components/NL4DV'
+
+import Charttable from '../components/Charttable'
+import Barchart1 from '../components/Barchart1'
+import Barchart2 from '../components/Barchart2'
+import Boxchart1 from '../components/Boxchart1'
+import Densitychart1 from '../components/Densitychart1'
 import Treechart from '../components/Treechart'
+import Histogramchart1 from '../components/Histogramchart1'
 import Correlationchart from '../components/Correlationchart'
 import Parallelchart from '../components/Parallelchart'
 import Scatterchart from '../components/Scatterchart'
 import ECDFchart from '../components/ECDFchart'
-import Spiderchart from '../components/Spiderchart'
+import Dropdown from '../components/Dropdown'
 
-import Barchart1 from '../components/Barchart1'
-import Barchart2 from '../components/Barchart2'
 import Barchart3 from '../components/Barchart3'
-import Boxchart1 from '../components/Boxchart1'
-import Boxchart2 from '../components/Boxchart2'
-import Densitychart1 from '../components/Densitychart1'
+import Spiderchart from '../components/Spiderchart'
 import Densitychart2 from '../components/Densitychart2'
-import Histogramchart1 from '../components/Histogramchart1'
-import Histogramchart2 from '../components/Histogramchart2'
+import Boxchart2 from '../components/Boxchart2'
 
 import { mainLayout2Style } from '../const'
 const PORT = 5000
 
 const Home = () => {
   const [data, setData] = useState([])
+  const [dataTreechart, setDataTreechart] = useState([])
   const [dataClassName, setClassName] = useState([])
   const [dataClassList, setClassList] = useState([])
+  const [dataModelList, setModelList] = useState([])
+  const [dataOutput1List, setOutput1List] = useState([])
+  const [dataOutput2List, setOutput2List] = useState([])
+  const [dataOutput3List, setOutput3List] = useState([])
 
-  const [dataBarchart2, setDataBarchart2] = useState([])
+  const [dataBarchart, setDataBarchart] = useState([])
   const [dataCharttable, setDataCharttable] = useState([])
   const [dataHistogramchart1, setHistogramchart1] = useState([])
   const [dataCorrelationchart, setCorrelationchart] = useState([])
@@ -45,51 +51,59 @@ const Home = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     axios
-    .post(`http://${window.location.hostname}:${PORT}/query?` + Math.random(), dataQuery)
-    .then(response => { setNL4DV(response.data) })
-    .catch(error => { alert(`ERROR - ${error.message}`) })
-  }
+      .post(`http://${window.location.hostname}:${PORT}/query?`, dataQuery)
+      .then(response => { setNL4DV(response.data) })
+      .catch(error => { alert(`ERROR - ${error.message}`) })
+    }
 
   useEffect(() => {
     axios
-      .get(`http://${window.location.hostname}:${PORT}/static/iris.json?` + Math.random())
+      .get(`http://${window.location.hostname}:${PORT}/static/iris.json?`)
       .then(response => { setData(response.data) })
       .catch(error => { alert(`ERROR - ${error.message}`) })
     axios
-      .get(`http://${window.location.hostname}:${PORT}/?` + Math.random())
+      .get(`http://${window.location.hostname}:${PORT}/static/treeData.json?`)
+      .then(response => { setDataTreechart(response.data) })
+      .catch(error => { alert(`ERROR - ${error.message}`) })
+    axios
+      .get(`http://${window.location.hostname}:${PORT}/?`)
       .then(response => {
         setClassName(response.data.className)
         setClassList(response.data.classList)
+        setModelList(response.data.modelList)
+        setOutput1List(response.data.dataOutput1List)
+        setOutput2List(response.data.dataOutput2List)
+        setOutput3List(response.data.dataOutput3List)
       })
       .catch(error => { alert(`ERROR - ${error.message}`) })    
 
     axios
-      .get(`http://${window.location.hostname}:${PORT}/barchart2?` + Math.random())
-      .then(response => { setDataBarchart2(response.data) })
+      .get(`http://${window.location.hostname}:${PORT}/barchart?`)
+      .then(response => { setDataBarchart(response.data) })
       .catch(error => { alert(`ERROR - ${error.message}`) })
     axios
-      .get(`http://${window.location.hostname}:${PORT}/charttable?` + Math.random())
+      .get(`http://${window.location.hostname}:${PORT}/charttable?`)
       .then(response => { setDataCharttable(response.data) })
       .catch(error => { alert(`ERROR - ${error.message}`) })
     axios
-      .get(`http://${window.location.hostname}:${PORT}/correlationchart?` + Math.random())
+      .get(`http://${window.location.hostname}:${PORT}/correlationchart?`)
       .then(response => { setCorrelationchart(response.data) })
       .catch(error => { alert(`ERROR - ${error.message}`) })
     axios
-      .get(`http://${window.location.hostname}:${PORT}/scatterchart?` + Math.random())
+      .get(`http://${window.location.hostname}:${PORT}/scatterchart?`)
       .then(response => { setScatterchart(response.data) })
       .catch(error => { alert(`ERROR - ${error.message}`) })
     axios
-      .get(`http://${window.location.hostname}:${PORT}/ECDFchart?` + Math.random())
+      .get(`http://${window.location.hostname}:${PORT}/ECDFchart?`)
       .then(response => { setECDFchart(response.data) })
       .catch(error => { alert(`ERROR - ${error.message}`) })  
     axios
-      .post(`http://${window.location.hostname}:${PORT}/histogramchart1?` + Math.random(), { 'row': 1, 'col': 1 })
+      .post(`http://${window.location.hostname}:${PORT}/histogramchart1?`, { 'row': 1, 'col': 1 })
       .then(response => { setHistogramchart1(response.data) })
       .catch(error => { alert(`ERROR - ${error.message}`) })
 
     axios
-      .get(`http://${window.location.hostname}:${PORT}/static/barchart1.json?` + Math.random())
+      .get(`http://${window.location.hostname}:${PORT}/static/barchart1.json?`)
       .then(response => { setData1(response.data) })
       .catch(error => { alert(`ERROR - ${error.message}`) })
   }, [])
@@ -100,23 +114,23 @@ const Home = () => {
         <div className="box" style={{ gridArea: 'dataset' }}>
           dataset
         </div>
-        <div className="box" style={{ gridArea: 'table-chart' }}>
+        <div className="box" style={{ gridArea: 'table-chart', overflow: 'auto' }}>
           <Tablechart data={data} />
         </div>
         <div className="box" style={{ gridArea: 'line-chart' }}>
           <Linechart data={data} />
         </div>
         <div className="box" style={{ gridArea: 'visualization' }}>
-          {/* <NL4DV spec={dataNL4DV} data={data} /> */}
           <form onSubmit={handleSubmit}>
             <input type="text" value={dataQuery} onChange={handleChange} placeholder="type your query here..." />
             <button type="submit">submit</button>
           </form>
+          <NL4DV spec={dataNL4DV} />
         </div>
 
         <div className="box" style={{ gridArea: 'chart-table-top-left' }}><Barchart1 data={data1} /></div>
-        <div className="box" style={{ gridArea: 'chart-table-top-right' }}><Barchart2 data={[dataBarchart2]} /></div>
-        <div className="box" style={{ gridArea: 'chart-table' }}>
+        <div className="box" style={{ gridArea: 'chart-table-top-right' }}><Barchart2 data={[dataBarchart]} /></div>
+        <div className="box" style={{ gridArea: 'chart-table', overflow: 'auto' }}>
           <Charttable
             data = {Array.from({ length: Object(dataCharttable.columnList).length }, (_, i) => ({
               key: Object.values(dataCharttable)[0][i],
@@ -136,9 +150,9 @@ const Home = () => {
         </div>
         <div
           className="box vertical-align-center"
-          style={{ gridArea: 'tree-chart' }}
+          style={{ gridArea: 'tree-chart', overflow: 'auto' }}
         >
-          <Treechart />
+          <Treechart data = {[dataTreechart]} />
         </div>
         <div
           className="box"
@@ -155,6 +169,9 @@ const Home = () => {
           <ECDFchart data={dataECDFchart} />
           <Parallelchart data={data} />
           <Scatterchart data={dataScatterchart} dataClassList={dataClassList} method={2} />
+        </div>
+        <div className="box" style={{ gridArea: 'interaction', overflow: 'visible' }}>
+          <Dropdown />
         </div>
         <div className="box" style={{ gridArea: 'right-charts' }}>
           <Barchart3 />
