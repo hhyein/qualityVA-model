f2836e4ccac0e8f57307c943e5233300ffa55b36
hhyein <48901291+hhyein@users.noreply.github.com>
2022-04-16 16:57:32 +0900

    Update const.js

--- templates/src/const.js
+++ templates/src/const.js
@@ -14,24 +14,24 @@ export const mainLayout2Style = {
   gridTemplateColumns: '300px 450px 450px 300px',
   gridTemplateRows: '80px 180px 100px 150px 250px 70px',
   gridTemplateAreas: `
-    'dataset chart-table-top-left chart-table-top-right right-charts'
-    'table-chart chart-table chart-table right-charts'
-    'table-chart tree-chart tree-chart right-charts'
-    'line-chart center-charts center-charts right-charts'
-    'visualization center-charts center-charts right-charts'
-    'visualization interaction interaction right-charts'
+    'dataset chart-table-top-left chart-table-top-right tree-chart'
+    'setting chart-table chart-table tree-chart'
+    'table-chart center-charts center-charts tree-chart'
+    'table-chart center-charts center-charts model'
+    'visualization center-charts center-charts model'
+    'visualization interaction interaction model'
   `,
 }
 
 export const boxTitles = {
-  dataset: '데이터셋 타이틀',
-  'table-chart': '테이블차트 타이틀',
-  'line-chart': '라인차트 타이틀',
-  visualization: 'visualization 타이틀',
-  'chart-table-top-left': '차트 테이블 좌측 위',
-  'chart-table-top-right': '차트 테이블 우측 위',
-  'tree-chart': '트리 차트',
-  'center-chart': '중앙 차트들',
-  interaction: 'interaction 타이틀',
-  'right-charts': '우측 차트들',
+  dataset: 'dataset upload',
+  setting: 'setting',
+  'table-chart': 'dataset',
+  visualization: 'nl4dv',
+  'chart-table-top-left': 'high correlation column',
+  'chart-table-top-right': 'class imbalance',
+  'center-chart': 'data correction',
+  interaction: 'action',
+  'tree-chart': 'action log',
+  'model': 'model accuracy'
 }
