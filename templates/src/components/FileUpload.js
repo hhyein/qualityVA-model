ee2a11213c632f36546ef76175e15b57bc246810
hhyein <48901291+hhyein@users.noreply.github.com>
2022-04-17 20:58:34 +0900

    Update FileUpload.js

--- templates/src/components/FileUpload.js
+++ templates/src/components/FileUpload.js
@@ -1,51 +1,38 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
-import Dropzone from "react-dropzone"
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Dropzone from 'react-dropzone'
 import Input from "./Input"
 import Button from "./Button"
 
-function Fileupload() {
+function Fileupload(){
   const [data, setData] = useState([])
   const PORT = 5000
 
   const dropHandler = (files) => {
-    var formData = new FormData()
-    const config = {
-      header: { "content-type": "multipart/form-data" },
-    }
-    formData.append("file", files[0])
-    axios
-      .post(
-        `http://${window.location.hostname}:${PORT}/fileUpload?` +
-          Math.random(),
-        formData,
-        config
-      )
-      .then((response) => {
-        console.log(response.data)
-      })
-      .catch((error) => {
-        alert(`ERROR - ${error.message}`)
-      })
+      var formData = new FormData();
+      const config = {
+        header : {'content-type' : 'multipart/form-data'}
+      }
+      formData.append("file", files[0])
+      axios
+        .post(`http://${window.location.hostname}:${PORT}/fileUpload?` + Math.random(), formData, config)
+        .then(response => { console.log(response.data) })
+        .catch(error => { alert(`ERROR - ${error.message}`) })
   }
 
-  return (
+  return(
     <div>
-      <Dropzone onDrop={dropHandler}>
-        {({ getRootProps, getInputProps }) => (
-          <div
-            style={{ display: "grid", gridAutoFlow: "column" }}
+      <Dropzone onDrop = {dropHandler}>
+        {({getRootProps, getInputProps}) => (
+          <div style={{ display: "grid", gridAutoFlow: "column" }}
             {...getRootProps()}
           >
-            <Input
-              disabled
-              style={{ borderRadius: "4px 0 0 4px", borderRightWidth: 0 }}
-            />
-            <Button style={{ borderRadius: "0 4px 4px 0" }}>
-              <input {...getInputProps()} />
-              Upload
-            </Button>
-          </div>
+          <Input disabled style={{ borderRadius: "4px 0 0 4px", borderRightWidth: 0 }} />
+          <Button style={{ borderRadius: "0 4px 4px 0" }}>
+            <input {...getInputProps()} />
+            Upload
+          </Button>
+        </div>
         )}
       </Dropzone>
     </div>
