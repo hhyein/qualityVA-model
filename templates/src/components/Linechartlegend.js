65352e605ce5f82e3ee7d807ddf16b7782b3cb43
hhyein <48901291+hhyein@users.noreply.github.com>
2022-04-20 21:38:59 +0900

    Add files via upload

---
+++ templates/src/components/Linechartlegend.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react'
+
+function Linechartlegend(props) {
+  const {data} = props
+  const svgRef = useRef()
+  const d3 = window.d3v4
+
+  useEffect(() => {
+    var svg = d3.select(svgRef.current);
+    d3.select(svgRef.current).selectAll("*").remove();
+
+    var margin = { top: 0, right: 0, bottom: 10, left: 0 },
+      width = 850 - margin.left - margin.right,
+      height = 30 - margin.top - margin.bottom
+
+    svg
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("circle").attr("cx", 10).attr("cy", 20).attr("r", 6).style("fill", "#eb3477")
+    svg.append("text").attr("x", 20).attr("y", 20).text("lr").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("circle").attr("cx", 75).attr("cy", 20).attr("r", 6).style("fill", "#8934eb")
+    svg.append("text").attr("x", 85).attr("y", 20).text("knn").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("circle").attr("cx", 145).attr("cy", 20).attr("r", 6).style("fill", "#4ceb34")
+    svg.append("text").attr("x", 155).attr("y", 20).text("nb").style("font-size", "15px").attr("alignment-baseline","middle")
+
+    }, [props.data]);
+
+  return (
+    <>
+      <svg ref = {svgRef}>
+      </svg>
+    </>
+  );
+}
+export default Linechartlegend
