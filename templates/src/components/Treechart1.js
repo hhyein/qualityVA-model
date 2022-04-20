8bdc15df24d2c727a144903c19c4a762de9b0b52
hhyein <48901291+hhyein@users.noreply.github.com>
2022-04-20 21:33:47 +0900

    Add files via upload

---
+++ templates/src/components/Treechart1.js
@@ -0,0 +1,136 @@
+import React, { useEffect, useRef } from 'react'
+
+function Treechart1() {
+  const svgRef = useRef()
+  const d3 = window.d3v3
+
+  var data = [
+    {
+      "index": "0",
+      "state": "none",
+      "name": "start",
+      "children": [
+        {
+          "index": "1",
+          "state": "none",
+          "name": "EM",
+          "children": [
+              {
+                "index": "2",
+                "state": "none",
+                "name": "LOCF",
+                "children": [
+                  {
+                    "index": "3",
+                    "state": "none",
+                    "name": "normalization",
+                    "children": [
+                      {
+                        "index": "4",
+                        "state": "none",
+                        "name": "remove",
+                        "children": [
+                          {
+                            "index": "5",
+                            "state": "current",
+                            "name": "remove",
+                            "children": [  
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+
+  useEffect(() => {
+    var margin = { top: 0, right: 0, bottom: 0, left: 0 },
+      width = 900 - margin.right - margin.left,
+      height = 300 - margin.top - margin.bottom
+
+    var i = 0
+
+    d3.select(svgRef.current).selectAll('*').remove()
+
+    var svg = d3
+      .select(svgRef.current)
+      .append('svg')
+      .attr('width', width + margin.right + margin.left)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+    var tree = d3.layout.tree().size([height, width])
+    var diagonal = d3.svg.diagonal().projection(function (d) {
+      return [d.y, d.x]
+    })
+
+    var root = data[0]
+    update(root)
+
+    function update() {
+      var nodes = tree.nodes(root).reverse()
+      var links = tree.links(nodes)
+
+      nodes.forEach(function (d) {
+        d.x = 10
+        d.y = d.depth * 85
+      })
+
+      var node = svg
+        .selectAll('g.node')
+        .data(nodes, function (d) {
+        return d.id || (d.id = ++i)
+      })
+
+      var nodeEnter = node
+        .enter()
+        .append('g')
+        .attr('class', 'node')
+        .attr('transform', function (d) {
+          return 'translate(' + d.y + ',' + d.x + ')'
+        })
+
+      nodeEnter
+        .append('circle')
+        .attr('r', 10)
+        .style('fill', function (d) {
+          if (d.state == 'current') {
+            return '#999999'
+          }
+          else {
+            return '#cccccc'
+          }
+        })
+
+      nodeEnter
+        .append('text')
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline', 'middle')
+        .text(function (d) {
+          return d.index
+        })
+
+      var link = svg
+        .selectAll('path.link')
+        .data(links)
+        .enter()
+        .insert('path', 'g')
+        .attr('class', 'link')
+        .attr('d', diagonal)
+    }
+  }, [])
+
+  return (
+    <div className = "svg-wrapper">
+      <svg ref = {svgRef}></svg>
+    </div>
+  )
+}
+export default Treechart1
