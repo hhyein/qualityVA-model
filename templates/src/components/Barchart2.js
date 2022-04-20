1d2be00210039f8f7018385a354855b02c396f18
hhyein <48901291+hhyein@users.noreply.github.com>
2022-04-20 21:35:43 +0900

    Update Barchart2.js

--- templates/src/components/Barchart2.js
+++ templates/src/components/Barchart2.js
@@ -1,27 +1,17 @@
 import React, { useEffect, useRef } from 'react'
 
 function Barchart2(props) {
-  const { data, dataColorCode } = props
+  const { data, dataClassList, dataColorCode } = props
   const svgRef = useRef()
   const d3 = window.d3v4
-
-  var subgroups = {}
-  Object.assign(subgroups, data[0])
-  subgroups = Object.keys(subgroups)
-  for(var i = 0; i < subgroups.length; i++) {
-    if(subgroups[i] === 'group')  {
-      subgroups.splice(i, 1);
-      i--;
-    }
-  }
-
+  
   useEffect(() => {
     var svg = d3.select(svgRef.current)
     d3.select(svgRef.current).selectAll('*').remove()
 
-    var margin = { top: 10, right: 10, bottom: 10, left: 10 },
-      width = 100 - margin.left - margin.right,
-      height = 30 - margin.top - margin.bottom
+    var margin = { top: 10, right: 0, bottom: 10, left: 0 },
+      width = 275 - margin.left - margin.right,
+      height = 80 - margin.top - margin.bottom
 
     svg
       .attr('width', width + margin.left + margin.right)
@@ -29,55 +19,43 @@ function Barchart2(props) {
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-    var x = d3.scaleLinear()
+    var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(data.map(function(d) { return d.class; }))
+      .padding(0.5)
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    var y = d3.scaleLinear()
       .domain([0, 100])
-      .range([0, width])
+      .range([ height, 0]);
+     
+    var color = d3.scaleOrdinal()
+      .domain(dataClassList)
+      .range(dataColorCode)  
 
-    var y = d3.scaleBand()
-      .range([0, height])
+    svg.selectAll("mybar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", function(d) { return x(d.class); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("fill", function(d) { return color(d.class); })
 
-    var color = d3
-      .scaleOrdinal()
-      .domain(subgroups)
-      .range(dataColorCode)
-
-    data.forEach(function (d) {
-      var tot = 0
-      for (var i in subgroups) {
-        var name = subgroups[i]
-        tot += +d[name]
-      }
-      for (var i in subgroups) {
-        var name = subgroups[i]
-        d[name] = (d[name] / tot) * 100
-      }
-    })
-
-    var stackedData = d3.stack().keys(subgroups)(data)
-
-    svg
-      .append('g')
-      .selectAll('g')
-      .data(stackedData)
+    svg.selectAll("mybar")
+      .data(data)
       .enter()
       .append('g')
-      .attr('fill', function (d) {
-        return color(d.key)
-      })
-      .selectAll('rect')
-      .data(function (d) {
-        return d
-      })
-      .enter()
-      .append('rect')
-      .attr('y', function (d) {
-        return y(d.data.group)
-      })
-      .attr('x', function (d) {
-        return x(d[0])
-      })
-      .attr('width', 100)
-      .attr('height', 30)
+      .append('text')
+      .attr('x', function(d) { return x(d.class) + x.bandwidth()/2 })
+      .attr('y', function(d) { return y(d.value) - 5 })
+      .text(function (d) { return d.value })
+      .attr('text-anchor', 'middle')
+      .attr('font-size', '10px');
+
   }, [props.data])
 
   return (
