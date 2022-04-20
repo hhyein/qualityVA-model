b2a0bce7795cf3fcac5a3977dd7a7b836b10d3f1
hhyein <48901291+hhyein@users.noreply.github.com>
2022-04-20 21:47:09 +0900

    Update Home.js

--- templates/src/pages/Home.js
+++ templates/src/pages/Home.js
@@ -1,27 +1,20 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 
-import Tablechart from "../components/Tablechart"
 import Fileupload from '../components/Fileupload'
 import Setting from '../components/Setting'
 import Input from "../components/Input"
 import Button from "../components/Button"
 import Radiobutton from "../components/Radiobutton"
 import NL4DV from "../components/NL4DV"
-import Charttable from "../components/Charttable"
 import Barchart1 from "../components/Barchart1"
 import Barchart2 from "../components/Barchart2"
-import Boxchart1 from "../components/Boxchart1"
-import Densitychart1 from "../components/Densitychart1"
 import Linechart from "../components/Linechart"
 import Linechartlegend from '../components/Linechartlegend'
 import Treechart1 from "../components/Treechart1"
-import Treechart2 from "../components/Treechart2"
 import Histogramchart1 from "../components/Histogramchart1"
 import Barchart1legend from '../components/Barchart1legend'
 import Heatmapchart from "../components/Heatmapchart"
-import Correlationchart from "../components/Correlationchart"
-import Parallelchart from "../components/Parallelchart"
 import Scatterchart from "../components/Scatterchart"
 import ECDFchart from "../components/ECDFchart"
 import Instance from "../components/Instance"
@@ -36,27 +29,20 @@ const PORT = 5000
 
 const Home = () => {
   const [data, setData] = useState([])
-  const [dataModelList, setModelList] = useState([])
-
-  const [dataFileName, setFileName] = useState([])
-  const [dataClassName, setClassName] = useState([])
   const [dataColumnList, setColumnList] = useState([])
   const [dataClassList, setClassList] = useState([])
   const [dataColorCode, setColorCode] = useState([])
   const [dataTypeList, setTypeList] = useState([])
   const [dataEvalList, setEvalList] = useState([])
 
+  const [dataBarchart1, setBarchart1] = useState([])
   const [dataBarchart2, setBarchart2] = useState([])
-  const [dataCharttable, setCharttable] = useState([])
   const [dataHistogramchart1, setHistogramchart1] = useState([])
-  const [dataCorrelationchart, setCorrelationchart] = useState([])
   const [dataScatterchart, setScatterchart] = useState([])
   const [dataECDFchart, setECDFchart] = useState([])
   const [dataHeatmapchart, setHeatmapchart] = useState([])
   const [dataHeatmapchartY, setHeatmapchartY] = useState([])
   const [dataLinechart, setLinechart] = useState([])
-
-  const [dataBarchart1, setBarchart1] = useState([])
 
   const [dataQuery, setQuery] = useState("")
   const [dataNL4DV, setNL4DV] = useState()
@@ -89,24 +75,11 @@ const Home = () => {
       .catch((error) => {
         alert(`ERROR - ${error.message}`)
       })
-    // axios
-    //   .get(
-    //     `http://${window.location.hostname}:${PORT}/static/modelData.json?` +
-    //       Math.random()
-    //   )
-    //   .then(response => {
-    //     setModelList(response.data)
-    //   })
-    //   .catch(error => {
-    //     alert(`ERROR - ${error.message}`)
-    //   })
 
     axios
       .get(`http://${window.location.hostname}:${PORT}/?` +
         Math.random())
       .then((response) => {
-        setFileName(response.data.fileName)
-        setClassName(response.data.className)
         setColumnList(response.data.columnList)
         setClassList(response.data.classList)
         setColorCode(
@@ -152,28 +125,6 @@ const Home = () => {
       )
       .then((response) => {
         setBarchart2(response.data.classDict)
-      })
-      .catch((error) => {
-        alert(`ERROR - ${error.message}`)
-      })
-    axios
-      .get(
-        `http://${window.location.hostname}:${PORT}/charttable?` +
-          Math.random()
-      )
-      .then((response) => {
-        setCharttable(response.data)
-      })
-      .catch((error) => {
-        alert(`ERROR - ${error.message}`)
-      })
-    axios
-      .get(
-        `http://${window.location.hostname}:${PORT}/correlationchart?` +
-          Math.random()
-      )
-      .then((response) => {
-        setCorrelationchart(response.data)
       })
       .catch((error) => {
         alert(`ERROR - ${error.message}`)
@@ -292,56 +243,6 @@ const Home = () => {
           <Linechartlegend />
           <Linechart data={dataLinechart} />
           <Treechart1 data={data} />
-          {/* <Charttable
-            data={Array.from(
-              { length: Object(dataCharttable.columnList).length },
-              (_, i) => ({
-                key: Object.values(dataCharttable)[0][i],
-                missing: (
-                  <Barchart1
-                    data={[Object.values(dataCharttable)[1][i]]}
-                    colorCode={'steelblue'}
-                  ></Barchart1>
-                ),
-                outlier: (
-                  <Barchart1
-                    data={[Object.values(dataCharttable)[2][i]]}
-                    colorCode={'orange'}
-                  ></Barchart1>
-                ),
-                icons: (
-                  <Barchart1
-                    data={[Object.values(dataCharttable)[3][i]]}
-                    colorCode={'darkgreen'}
-                  ></Barchart1>
-                ),
-                quantile: (
-                  <Boxchart1
-                    data={[{ Value: Object.values(dataCharttable)[4][i] }]}
-                  ></Boxchart1>
-                ),
-                descriptive: (
-                  <Densitychart1
-                    data={[{ Value: Object.values(dataCharttable)[5][i] }]}
-                  ></Densitychart1>
-                ),
-              })
-            )}
-            onClick={(rowIdx, colIdx) =>
-              axios
-                .post(
-                  `http://${window.location.hostname}:${PORT}/histogramchart1?` +
-                    Math.random(),
-                  { row: rowIdx, col: colIdx }
-                )
-                .then((response) => {
-                  setHistogramchart1(response.data)
-                })
-                .catch((error) => {
-                  alert(`ERROR - ${error.message}`)
-                })
-            }
-          /> */}
         </Box>
         <Box
           title="instance-level"
@@ -371,8 +272,6 @@ const Home = () => {
             gridGap: "10px",
           }}
         >
-          {/* <Parallelchart data={data} /> */}
-          {/* <Correlationchart data={dataCorrelationchart} /> */}
           <Scatterchart
             data={dataScatterchart}
             method={1}
@@ -402,7 +301,6 @@ const Home = () => {
           <Treetable />
         </Box>
         <Box title="model" style={{ overflow: "auto" }}>
-          {/* <Tablechart data={dataModelList} /> */}
         </Box>
         <Box title="visualization">
           <form
@@ -420,6 +318,7 @@ const Home = () => {
               submit
             </Button>
           </form>
+          <NL4DV spec={dataNL4DV} />
         </Box>        
       </div>
     </div>
