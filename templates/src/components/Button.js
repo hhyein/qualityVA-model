c7e3ac6e277aebd3b6ef1a3ecff1eabf92a74899
hhyein <48901291+hhyein@users.noreply.github.com>
2022-04-21 21:02:36 +0900

    Update Button.js

--- templates/src/components/Button.js
+++ templates/src/components/Button.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function Button({ style, children, otherProps }) {
+export default function Button({ style, children, ...otherProps }) {
   return (
     <button
       style={{
