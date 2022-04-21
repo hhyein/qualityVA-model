b5306d590081ffc3f929525d92b28eb8bdd97deb
yejin <yeajin357@naver.com>
2022-04-21 21:16:08 +0900

    Modify color to colorCode

--- templates/src/components/modules/dataOverview/index.js
+++ templates/src/components/modules/dataOverview/index.js
@@ -61,7 +61,7 @@ export default function DataOverview({ columnList }) {
         data={heatmapChartData}
         yList={heatmapChartYList}
         columnList={columnList}
-        color={heatmapColor}
+        colorCode={heatmapColor}
       />
     </Box>
   )
