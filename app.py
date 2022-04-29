feebc2ea2da934f4741a400fa07a2a30c8f520da
hhyein <gumdung98@naver.com>
2022-04-29 21:30:53 +0900

    Create chart table dataset to test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import json
 import codecs
 import numpy as np
 import pandas as pd
-from nl4dv import NL4DV
+# from nl4dv import NL4DV
 from io import StringIO
 from scipy import stats
 from collections import Counter
@@ -95,59 +95,60 @@ def setting():
 def query():
   query = request.get_data().decode('utf-8')
 
-  global originDf
-  originDf = originDf.reindex(sorted(originDf.columns), axis = 1)
+  # global originDf
+  # originDf = originDf.reindex(sorted(originDf.columns), axis = 1)
 
-  nl4dvDict = originDf.dropna().to_dict('records')
-  nl4dvInstance = NL4DV(data_url = os.path.join(filePath))
-  nl4dvInstance.set_dependency_parser(config = {"name": "spacy", "model": "en_core_web_sm", "parser": None})
-  nl4dvOutput = nl4dvInstance.analyze_query(query)
+  # nl4dvDict = originDf.dropna().to_dict('records')
+  # nl4dvInstance = NL4DV(data_url = os.path.join(filePath))
+  # nl4dvInstance.set_dependency_parser(config = {"name": "spacy", "model": "en_core_web_sm", "parser": None})
+  # nl4dvOutput = nl4dvInstance.analyze_query(query)
 
-  # extraction attribute, task, vistype
-  try:
-    attributes = nl4dvOutput['visList'][0]['attributes']
-    tasks = nl4dvOutput['visList'][0]['tasks']
-    visType = nl4dvOutput['visList'][0]['visType']
-  except:
-    return jsonify({'nl4dv': 'please writing valid query'})
+  # # extraction attribute, task, vistype
+  # try:
+  #   attributes = nl4dvOutput['visList'][0]['attributes']
+  #   tasks = nl4dvOutput['visList'][0]['tasks']
+  #   visType = nl4dvOutput['visList'][0]['visType']
+  # except:
+  #   return jsonify({'nl4dv': 'please writing valid query'})
 
-  if type(attributes) == list:
-    attributes = ",".join(attributes)
-  if type(tasks) == list:
-    tasks = ",".join(tasks)
-  if type(visType) == list:
-    visType = ",".join(visType)
+  # if type(attributes) == list:
+  #   attributes = ",".join(attributes)
+  # if type(tasks) == list:
+  #   tasks = ",".join(tasks)
+  # if type(visType) == list:
+  #   visType = ",".join(visType)
 
-  # extraction vlspec
-  vlSpec = nl4dvOutput['visList'][0]['vlSpec']
-  vlSpec['data']['values'] = nl4dvDict
+  # # extraction vlspec
+  # vlSpec = nl4dvOutput['visList'][0]['vlSpec']
+  # vlSpec['data']['values'] = nl4dvDict
 
-  vlSpec['width'] = "container"
-  vlSpec['height'] = "container"
+  # vlSpec['width'] = "container"
+  # vlSpec['height'] = "container"
 
-  # preprocessing vlspec
-  if 'encoding' in vlSpec:
-      if 'x' in vlSpec['encoding']:
-          if 'aggregate' in vlSpec['encoding']['x']:
-              del vlSpec['encoding']['x']['aggregate']
-  if 'encoding' in vlSpec:
-      if 'y' in vlSpec['encoding']:
-          if 'aggregate' in vlSpec['encoding']['y']:
-              del vlSpec['encoding']['y']['aggregate']
-  if 'encoding' in vlSpec:
-      if 'x' in vlSpec['encoding']:
-          if 'bin' in vlSpec['encoding']['x']:
-              del vlSpec['encoding']['x']['bin']
-  if 'encoding' in vlSpec:
-      if 'color' in vlSpec['encoding']:
-          if 'aggregate' in vlSpec['encoding']['color']:
-              del vlSpec['encoding']['color']['aggregate']
+  # # preprocessing vlspec
+  # if 'encoding' in vlSpec:
+  #     if 'x' in vlSpec['encoding']:
+  #         if 'aggregate' in vlSpec['encoding']['x']:
+  #             del vlSpec['encoding']['x']['aggregate']
+  # if 'encoding' in vlSpec:
+  #     if 'y' in vlSpec['encoding']:
+  #         if 'aggregate' in vlSpec['encoding']['y']:
+  #             del vlSpec['encoding']['y']['aggregate']
+  # if 'encoding' in vlSpec:
+  #     if 'x' in vlSpec['encoding']:
+  #         if 'bin' in vlSpec['encoding']['x']:
+  #             del vlSpec['encoding']['x']['bin']
+  # if 'encoding' in vlSpec:
+  #     if 'color' in vlSpec['encoding']:
+  #         if 'aggregate' in vlSpec['encoding']['color']:
+  #             del vlSpec['encoding']['color']['aggregate']
 
-  del vlSpec['mark']['tooltip']
-  del vlSpec['data']['format']
-  del vlSpec['data']['url']
+  # del vlSpec['mark']['tooltip']
+  # del vlSpec['data']['format']
+  # del vlSpec['data']['url']
 
-  return jsonify({'nl4dv': vlSpec})
+  # return jsonify({'nl4dv': vlSpec})
+  return json.dumps({'nl4dv': 'success'})
 
 @app.route('/lineChart', methods=['GET', 'POST'])
 def lineChart():
@@ -168,6 +169,52 @@ def treeChart():
   response['treeLength'] = currentCnt
 
   return jsonify(response)
+
+# autoML
+@app.route('/chartTable', methods = ['GET', 'POST'])
+def chartTable():
+  # global inputModelList, inputEvalList
+  # combinationList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+  # # have to develop - combination
+  # with open('static/modelData.json') as data:
+  #   data = json.load(data)
+  # autoMLDf = pd.DataFrame.from_dict(data)
+
+  # modelList = autoMLDf['Model'].values.tolist()
+  # modelIndex = []
+  # for i in inputModelList:
+  #   modelIndex.append(modelList.index(i))
+
+  # evalList = ['Accuracy', 'AUC', 'Recall', 'Prec.', 'F1', 'Kappa', 'MCC', 'TT (Sec)']
+  # evalIndex = []
+  # for i in inputEvalList:
+  #   evalIndex.append(evalList.index(i) + 1)
+
+  # resultList = []
+  # for i in evalIndex:
+  #   tmpList = []
+  #   for j in modelIndex:
+  #     tmpList.append(autoMLDf.iloc[j][i])
+  #   resultList.append(tmpList[0])
+
+  # response = {}
+  # response['combinationList'] = combinationList
+  # response['inputModelList'] = inputModelList
+  # response['inputEvalList'] = inputEvalList
+
+  # for i in range(len(inputEvalList)):
+  #   tmpList = []
+  #   for j in range(len(combinationList)):
+  #     data = resultList[i]
+  #     originData = 1.0 - resultList[i]
+
+  #     tmpList.append({'data': data, 'originData': originData})
+  #   response[inputEvalList[i]] = tmpList
+
+  response = {'combinationList': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 'inputModelList': ['Logistic Regression'], 'inputEvalList': ['Accuracy', 'AUC', 'Recall'], 'Accuracy': [{'data': 0.9553, 'originData': 0.04469999999999996}, {'data': 0.9, 'originData': 0.1}, {'data': 0.8, 'originData': 0.2}, {'data': 0.95, 'originData': 0.05}, {'data': 0.7, 'originData': 0.3}, {'data': 0.75, 'originData': 0.25}, {'data': 0.95, 'originData': 0.05}, {'data': 0.77, 'originData': 0.23}, {'data': 0.9553, 'originData': 0.04469999999999996}, {'data': 0.95, 'originData': 0.05}], 'AUC': [{'data': 0.9, 'originData': 0.1}, {'data': 0.6, 'originData': 0.4}, {'data': 0.8, 'originData': 0.2}, {'data': 0.95, 'originData': 0.05}, {'data': 0.7, 'originData': 0.3}, {'data': 0.75, 'originData': 0.25}, {'data': 0.95, 'originData': 0.05}, {'data': 0.77, 'originData': 0.23}, {'data': 0.9553, 'originData': 0.04469999999999996}, {'data': 0.95, 'originData': 0.05}], 'Recall': [{'data': 0.9553, 'originData': 0.04469999999999996}, {'data': 0.9, 'originData': 0.1}, {'data': 0.8, 'originData': 0.2}, {'data': 0.95, 'originData': 0.05}, {'data': 0.7, 'originData': 0.3}, {'data': 0.75, 'originData': 0.25}, {'data': 0.95, 'originData': 0.05}, {'data': 0.77, 'originData': 0.23}, {'data': 0.9553, 'originData': 0.04469999999999996}, {'data': 0.95, 'originData': 0.05}]}
+
+  return json.dumps(response)
 
 @app.route('/modelDetailTable', methods = ['GET', 'POST'])
 def modelDetailTable():
@@ -226,57 +273,6 @@ def modelDetailTable():
   response['actionDetailList'] = actionDetailList
   response['barChartList'] = barChartList
   response['densityChartList'] = densityChartList
-
-  return json.dumps(response)
-
-# autoML
-@app.route('/chartTable', methods = ['GET', 'POST'])
-def chartTable():
-  combinationList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-  inputModelList = ['Logistic Regression']
-  inputEvalList = ['Accuracy', 'AUC', 'Recall']
-
-  # global originDf, predictName
-  # automlDf = originDf.dropna()
-  # clf = setup(data = automlDf, target = predictName, preprocess = False, session_id = 42, silent = True)
-  # models = compare_models()
-  # results = pull()
-  # results.to_json('static/modelData.json', orient = 'records', indent = 4)
-
-  with open('static/modelData.json') as data:
-    data = json.load(data)
-  autoMLDf = pd.DataFrame.from_dict(data)
-
-  modelList = autoMLDf['Model'].values.tolist()
-  modelIndex = []
-  for i in inputModelList:
-    modelIndex.append(modelList.index(i))
-
-  evalList = ['Accuracy', 'AUC', 'Recall', 'Prec.', 'F1', 'Kappa', 'MCC', 'TT (Sec)']
-  evalIndex = []
-  for i in inputEvalList:
-    evalIndex.append(evalList.index(i) + 1)
-
-  resultList = []
-  for i in evalIndex:
-    tmpList = []
-    for j in modelIndex:
-      tmpList.append(autoMLDf.iloc[j][i])
-    resultList.append(tmpList[0])
-
-  response = {}
-  response['combinationList'] = combinationList
-  response['inputModelList'] = inputModelList
-  response['inputEvalList'] = inputEvalList
-
-  for i in range(len(inputEvalList)):
-    tmpList = []
-    for j in range(len(combinationList)):
-      data = resultList[i]
-      originData = 1.0 - resultList[i]
-
-      tmpList.append({'data': data, 'originData': originData})
-    response[inputEvalList[i]] = tmpList
 
   return json.dumps(response)
 
