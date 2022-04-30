54a161b8ebf077b142da5a520c5068d4c4436514
yejin <yeajin357@naver.com>
2022-04-30 17:12:27 +0900

    Add table interaction

--- templates/src/components/modules/modelOverview/ChartTable.js
+++ templates/src/components/modules/modelOverview/ChartTable.js
@@ -1,42 +1,51 @@
-import React from 'react'
+import React from "react"
 
-export default function ChartTable(props) {
-  const { data = [], onTableCellClick } = props
-
-  const columnKeys = Object.keys(data[0]).slice(1)
+export default function ChartTable({
+  data = [],
+  onTableCellClick,
+  onTableHeadClick,
+  canSortColumns,
+  selectedColumn,
+}) {
   console.log(data)
+  const columnKeys = Object.keys(data[0]).slice(1)
+
   return data.length > 0 ? (
     <div
       style={{
-        display: 'grid',
-        gridTemplateColumns: `auto auto auto repeat(${
-          columnKeys.length - 2
-        }, 1fr)`,
+        display: "grid",
+        gridTemplateColumns: `auto auto auto repeat(${canSortColumns.length}, 1fr)`,
       }}
     >
       <div className="grid-th" />
-      {columnKeys.map((key, i) => (
-        <div
-          className="grid-th"
-          key={key}
-          style={{
-            textAlign: 'center',
-            fontWeight: 'bold',
-            borderRight: i === columnKeys.length - 1 ? 'none' : undefined,
-          }}
-        >
-          {key}
-        </div>
-      ))}
+      {columnKeys.map((key, i) => {
+        const isSortButton = canSortColumns.includes(key)
+        return (
+          <div
+            className="grid-th"
+            key={key}
+            style={{
+              cursor: isSortButton ? "pointer" : "default",
+              background: selectedColumn === key ? "#e1e1e1" : undefined,
+              textAlign: "center",
+              fontWeight: "bold",
+              borderRight: i === columnKeys.length - 1 ? "none" : undefined,
+            }}
+            onClick={() => isSortButton && onTableHeadClick(key)}
+          >
+            {key}
+          </div>
+        )
+      })}
       {data.map(({ key, ...others }, rowIdx) => {
         const isLastRow = rowIdx === data.length - 1
         return (
-          <React.Fragment key={rowIdx}>
+          <React.Fragment key={key}>
             <div
               className="grid-td"
               style={{
-                fontWeight: 'bold',
-                borderBottom: isLastRow ? 'none' : undefined,
+                fontWeight: "bold",
+                borderBottom: isLastRow ? "none" : undefined,
               }}
             >
               {key}
@@ -44,12 +53,12 @@ export default function ChartTable(props) {
             {Object.values(others).map((chart, colIdx) => (
               <div
                 className="grid-td"
-                key={`${rowIdx}${colIdx}`}
+                key={`${key}${colIdx}`}
                 onClick={() => onTableCellClick(rowIdx)}
                 style={{
                   borderRight:
-                    colIdx === columnKeys.length - 1 ? 'none' : undefined,
-                  borderBottom: isLastRow ? 'none' : undefined,
+                    colIdx === columnKeys.length - 1 ? "none" : undefined,
+                  borderBottom: isLastRow ? "none" : undefined,
                 }}
               >
                 {chart}
--- templates/src/components/modules/modelOverview/index.js
+++ templates/src/components/modules/modelOverview/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { Box } from "../../Box"
 import ChartTable from "./ChartTable"
 import HorizontalBarChart from "../../charts/HorizontalBarChart"
@@ -9,77 +9,78 @@ export default function ModelOverview() {
     useFileData()
   const { chartTable } = modelOverviewData
 
+  const [data, setData] = useState([])
+  const [selectedColumn, setSelectedColumn] = useState()
+
+  useEffect(() => {
+    if (!chartTable) {
+      return
+    }
+    setData(
+      chartTable.combinationList.map((combination, i) => ({
+        key: combination,
+        combination: ["missing", "outlier", "inconsistent", "transformation"], // chartTable.combinationIconList[i]
+        combinationDetail: ["lof", "em", "rem", "std"], // chartTable.combinationDetailIconList[i]
+        ...chartTable.inputEvalList.reduce(
+          (acc, cur) => ({
+            ...acc,
+            [cur]: chartTable[cur][i],
+          }),
+          {}
+        ),
+      }))
+    )
+  }, [chartTable])
+
+  const handleTableHeadClick = useCallback((columnName) => {
+    setSelectedColumn(columnName)
+    setData((prev) =>
+      prev.sort((a, b) => b[columnName].data - a[columnName].data)
+    )
+  }, [])
+
   return (
     <Box title="model-overview" style={{ overflow: "auto" }}>
-      {!isEmptyData({ chartTable }) && (
+      {!isEmptyData({ chartTable }) && data.length > 0 && (
         <ChartTable
-          onTableCellClick={(rowIdx) => setSelectedModelOverviewTableRow(rowIdx)}
-          data={Array.from(
-            { length: Object(chartTable.combinationList).length },
-            (_, i) => ({
-              key: i,
-              combination: (
-                <div style={{ display: "flex" }}>
-                  <img
-                    src={require("../../icons/missing.png")}
-                    alt={""}
-                    style={{ height: "25px", width: "25px" }}
-                  ></img>
-                  <img
-                    src={require("../../icons/outlier.png")}
-                    alt={""}
-                    style={{ height: "25px", width: "25px" }}
-                  ></img>
-                  <img
-                    src={require("../../icons/inconsistent.png")}
-                    alt={""}
-                    style={{ height: "25px", width: "25px" }}
-                  ></img>
-                  <img
-                    src={require("../../icons/transformation.png")}
-                    alt={""}
-                    style={{ height: "25px", width: "25px" }}
-                  ></img>
-                </div>
-              ),
-              combinationDetail: (
-                <div style={{ display: "flex" }}>
-                  <img
-                    src={require("../../icons/lof.png")}
-                    alt={""}
-                    style={{ height: "25px", width: "25px" }}
-                  ></img>
-                  <img
-                    src={require("../../icons/em.png")}
-                    alt={""}
-                    style={{ height: "25px", width: "25px" }}
-                  ></img>
-                  <img
-                    src={require("../../icons/rem.png")}
-                    alt={""}
-                    style={{ height: "25px", width: "25px" }}
-                  ></img>
-                  <img
-                    src={require("../../icons/std.png")}
-                    alt={""}
-                    style={{ height: "25px", width: "25px" }}
-                  ></img>
-                </div>
-              ),
-              ...chartTable.inputEvalList.reduce(
-                (acc, cur, j) => ({
-                  ...acc,
-                  [cur]: (
-                    <HorizontalBarChart
-                      data={[Object.values(chartTable)[j + 3][i]]}
-                      colorCode={["lightcoral", "mediumturquoise", "sienna"][j]}
-                    />
-                  ),
-                }),
-                {}
-              ),
-            })
-          )}
+          canSortColumns={chartTable.inputEvalList}
+          selectedColumn={selectedColumn}
+          onTableHeadClick={handleTableHeadClick}
+          onTableCellClick={(rowIdx) =>
+            setSelectedModelOverviewTableRow(rowIdx)
+          }
+          data={data.map((d) => ({
+            key: d.key,
+            ...["combination", "combinationDetail"].reduce(
+              (acc, cur) => ({
+                ...acc,
+                [cur]: (
+                  <div style={{ display: "flex" }}>
+                    {d[cur].map((imgName) => (
+                      <img
+                        src={require(`../../icons/${imgName}.png`)}
+                        alt={""}
+                        style={{ height: "25px", width: "25px" }}
+                      />
+                    ))}
+                  </div>
+                ),
+              }),
+              {}
+            ),
+            ...chartTable.inputEvalList.reduce(
+              (acc, cur, j) => ({
+                ...acc,
+                [cur]: (
+                  <HorizontalBarChart
+                    data={[d[cur]]}
+                    colorCode={["lightcoral", "mediumturquoise", "sienna"][j]}
+                  />
+                ),
+              }),
+              {}
+            ),
+          }))}
         />
       )}
     </Box>
