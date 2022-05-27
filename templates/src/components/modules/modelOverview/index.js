0e912f65abfeaafa360866d42ce9cab38a30e5b6
Yejin <yeajin357@naver.com>
2022-05-27 17:09:57 +0900

    use default selectedColumn

--- templates/src/components/modules/modelOverview/index.js
+++ templates/src/components/modules/modelOverview/index.js
@@ -1,11 +1,12 @@
-import React, { useCallback, useEffect, useState } from "react"
-import { Box } from "../../Box"
-import ChartTable from "./ChartTable"
-import HorizontalBarChart from "../../charts/HorizontalBarChart"
-import { useFileData } from "../../../contexts/FileDataContext"
+import React, { useCallback, useEffect, useState } from 'react'
+import { Box } from '../../Box'
+import ChartTable from './ChartTable'
+import HorizontalBarChart from '../../charts/HorizontalBarChart'
+import { useFileData } from '../../../contexts/FileDataContext'
 
 export default function ModelOverview() {
-  const { modelOverviewData, isEmptyData, setSelectedModelOverviewTableRow } = useFileData()
+  const { modelOverviewData, isEmptyData, setSelectedModelOverviewTableRow } =
+    useFileData()
   const { chartTable } = modelOverviewData
 
   const [data, setData] = useState([])
@@ -15,59 +16,62 @@ export default function ModelOverview() {
     if (!chartTable) {
       return
     }
+    const chartTableData = chartTable.combinationList.map((combination, i) => ({
+      key: combination,
+      model: chartTable.modelNames[i],
+      combination: chartTable.combinationIconList[i],
+      combinationDetail: chartTable.combinationDetailIconList[i],
+      ...chartTable.inputEvalList.reduce(
+        (acc, cur) => ({
+          ...acc,
+          [cur]: chartTable[cur][i],
+        }),
+        {}
+      ),
+    }))
+    const defaultSelectedColumn = chartTable.inputEvalList[0]
+    setSelectedColumn(defaultSelectedColumn)
     setData(
-      chartTable.combinationList.map((combination, i) => ({
-        key: combination,
-        model: chartTable.modelNames[i],
-        combination: chartTable.combinationIconList[i],
-        combinationDetail: chartTable.combinationDetailIconList[i],
-        ...chartTable.inputEvalList.reduce(
-          (acc, cur) => ({
-            ...acc,
-            [cur]: chartTable[cur][i],
-          }),
-          {}
-        ),
-      }))
+      chartTableData.sort(
+        (a, b) => b[defaultSelectedColumn].data - a[defaultSelectedColumn].data
+      )
     )
   }, [chartTable])
 
-  const handleTableHeadClick = useCallback((columnName) => {
+  const handleTableHeadClick = useCallback(columnName => {
     setSelectedColumn(columnName)
-    setData((prev) =>
+    setData(prev =>
       prev.sort((a, b) => b[columnName].data - a[columnName].data)
     )
   }, [])
 
   return (
-    <Box title="model-overview" style={{ overflow: "auto" }}>
+    <Box title="model-overview" style={{ overflow: 'auto' }}>
       {!isEmptyData({ chartTable }) && data.length > 0 && (
         <ChartTable
           canSortColumns={chartTable.inputEvalList}
           selectedColumn={selectedColumn}
           onTableHeadClick={handleTableHeadClick}
-          onTableCellClick={(rowIdx) =>
-            setSelectedModelOverviewTableRow(rowIdx)
-          }
-          data={data.map((d) => ({
+          onTableCellClick={rowIdx => setSelectedModelOverviewTableRow(rowIdx)}
+          data={data.map(d => ({
             key: d.key,
-            ...["model"].reduce(
+            ...['model'].reduce(
               (acc, cur) => ({
                 ...acc,
                 [cur]: d[cur],
               }),
               {}
             ),
-            ...["combination", "combinationDetail"].reduce(
+            ...['combination', 'combinationDetail'].reduce(
               (acc, cur) => ({
                 ...acc,
                 [cur]: (
-                  <div style={{ display: "flex" }}>
-                    {d[cur].map((imgName) => (
+                  <div style={{ display: 'flex' }}>
+                    {d[cur].map(imgName => (
                       <img
                         src={require(`../../icons/${imgName}.png`)}
-                        alt={""}
-                        style={{ height: "25px", width: "25px" }}
+                        alt={''}
+                        style={{ height: '25px', width: '25px' }}
                       />
                     ))}
                   </div>
@@ -81,7 +85,7 @@ export default function ModelOverview() {
                 [cur]: (
                   <HorizontalBarChart
                     data={[d[cur]]}
-                    colorCode={["lightcoral", "mediumturquoise", "sienna"][j]}
+                    colorCode={['lightcoral', 'mediumturquoise', 'sienna'][j]}
                   />
                 ),
               }),
